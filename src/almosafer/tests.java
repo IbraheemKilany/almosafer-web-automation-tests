94f3042d7857ef4ae463c9ad4d0ffc370337de89
ibraheem <kilany970@gmail.com>
2023-12-06 02:27:30 +0300

    new update

---
+++ src/almosafer/tests.java
@@ -0,0 +1,59 @@
+package almosafer;
+
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class tests {
+	
+	WebDriver driver = new ChromeDriver();
+	String url ="https://global.almosafer.com/en";
+	
+	
+	
+	@BeforeTest
+	public void beforetest() {
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4000));// هذا الكود عشان لو صارت مشكلة معينه يستنى لحتى يحلها 
+		driver.get(url);
+		driver.manage().window().maximize();
+		driver.findElement(By.xpath("/html/body/div[3]/div/div[1]/div/div/div/button[1]")).click();
+	}
+
+	@Test()
+
+	public void tochecklanguege() {
+		String language= driver.findElement(By.tagName("html")).getAttribute("lang");
+		Assert.assertEquals(language, "en");
+	}
+	@Test()
+	public void tocheckcurrency() {
+		
+		String curency =driver.findElement(By.xpath("//button[normalize-space()='SAR']")).getText();
+		Assert.assertEquals(curency, "SAR");
+			
+		
+	}
+	
+		
+		
+		
+	
+		
+	
+	
+	
+
+	@AfterTest
+	public void aftertest() {
+	}
+
+}
